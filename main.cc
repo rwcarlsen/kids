1e20668ef3156c2e022f521cbcb38e13240e92b0
Robert Carlsen <rwcarlsen@gmail.com>
2013-08-25 13:21:12 -0500

    spacebar to toggle between upper and lower case

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <cctype>
+#include <locale>
 
 #include "sdl/sdl.h"
 
@@ -26,6 +27,9 @@ int main(int argc, char** argv) {
     // start up the main loop
     SDL_Event ev;
     bool done = false;
+    bool big = true;
+    std::string curr_k("A");
+    sdl::Color curr_color = sdl::Color::purple();
     while (!done) {
       // process events
       while (SDL_PollEvent(&ev)) {
@@ -34,24 +38,36 @@ int main(int argc, char** argv) {
         } else if (ev.type == SDL_KEYDOWN) {
           if (ev.key.keysym.sym == SDLK_ESCAPE) {
             done = true;
+          } else if (ev.key.keysym.sym == SDLK_SPACE) {
+            big = !big;
+            char c;
+            if (big) {
+              c = std::toupper(curr_k.c_str()[0]);
+            } else {
+              c = std::tolower(curr_k.c_str()[0]);
+            }
+            curr_k[0] = c;
+          } else if (ev.key.keysym.sym == SDLK_RETURN) {
+            
+          } else {
+            std::string tmp(SDL_GetKeyName(ev.key.keysym.sym));
+            if (tmp.size() == 1 && std::isalnum(tmp.c_str()[0])) {
+              curr_k = tmp;
+            }
           }
-          const char* k = SDL_GetKeyName(ev.key.keysym.sym);
-          if (!std::isalpha(k[0] && !std::isdigit(k[0]))) {
-            continue;
-          }
 
-          auto surf = font.RenderBlended(std::string(k), sdl::Color::purple());
-
-          int sw = surf->width();
-          int sh = surf->height();
-          SDL_Rect dst = {(w - sw) / 2, (h - sh) / 2, sw, sh};
-
-          ren.Clear();
-          sdl::Texture tex(ren, *surf.get());
-          tex.ApplyRects(NULL, &dst);
-          ren.Render();
         }
       }
+      auto surf = font.RenderBlended(curr_k, curr_color);
+
+      int sw = surf->width();
+      int sh = surf->height();
+      SDL_Rect dst = {(w - sw) / 2, (h - sh) / 2, sw, sh};
+
+      ren.Clear();
+      sdl::Texture tex(ren, *surf.get());
+      tex.ApplyRects(NULL, &dst);
+      ren.Render();
       SDL_Delay(20);
     }
 
